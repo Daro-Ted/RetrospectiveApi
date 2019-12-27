764c818575a802e28f8faca6a6c6b60ab1e91c39
HO-Ted <da_rossy@hotmail.com>
2019-12-27 09:40:20 +0000

    Add files via upload

--- MappingProfile/MapProfile.cs
+++ MappingProfile/MapProfile.cs
@@ -1,1 +1,29 @@
+﻿using AutoMapper;
+using RetrospectiveApi.Entities;
+using RetrospectiveApi.Models;
+using RetrospectiveApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace RetrospectiveApi.MappingProfile
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<RetropsectiveProject, ProjectDto>();
+            CreateMap<ProjectDto,RetropsectiveProject>();
+            CreateMap<Feedback, FeedbackDto>();
+            CreateMap<FeedbackDto, Feedback>();
+            CreateMap<RetrospectiveWithoutFeedback, ProjectDto>();
+            CreateMap<ProjectDto, RetrospectiveWithoutFeedback>();
+            CreateMap<ProjectDto, RetrospectiveViewModel>();
+            CreateMap<RetrospectiveViewModel, ProjectDto>();
+            CreateMap<FeedbackViewModel, FeedbackDto>();
+            CreateMap<FeedbackDto, FeedbackViewModel>();
+        }
+    }
+}
