c6d544238809cc9170fe54fdc274640fd4fad5b1
HO-Ted <da_rossy@hotmail.com>
2019-12-27 09:32:21 +0000

    Add files via upload

---
+++ Repositories/IRetrospectiveRepository.cs
@@ -0,0 +1,22 @@
+﻿using RetrospectiveApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrospectiveApi.Repositories
+{
+    public interface IRetrospectiveRepository
+    {
+        bool ProjectNameExist(string name);
+        bool ProjectExist(int id);
+        RetropsectiveProject GetProject(int id, bool IncludeFeedback);
+        IEnumerable<RetropsectiveProject> GetProjects();
+        IEnumerable<RetropsectiveProject> GetProjectByDate(DateTime date);
+        void AddProject(RetropsectiveProject project);
+        //void AddProjectWithoutFeedback(RetrospectiveProjectWithOutFeedback projectWithoutFeedback);
+        void AddFeedback(int projectId, Feedback feedbacks);
+        bool Save();
+    }
+}
--- Repositories/RetrospectiveRepository.cs
+++ Repositories/RetrospectiveRepository.cs
@@ -1,1 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RetrospectiveApi.Entities;
 
+namespace RetrospectiveApi.Repositories
+{
+    public class RetrospectiveRepository : IRetrospectiveRepository
+    {
+        private readonly ProjectDBContext _context;
+
+        public RetrospectiveRepository(ProjectDBContext context)
+        {
+            _context = context;
+        }
+
+        public void AddFeedback(int projectId, Feedback feedbacks)
+        {
+            var project = GetProject(projectId, false);
+            project.Feedbacks.Add(feedbacks);
+        }
+
+        public void AddProject(RetropsectiveProject project)
+        {
+            _context.RetropsectiveProjects.Add(project);
+        }
+
+        public RetropsectiveProject GetProject(int projectId, bool IncludeFeedback)
+        {
+            if (IncludeFeedback)
+            {
+                return _context.RetropsectiveProjects
+                    .Include(p => p.Feedbacks).FirstOrDefault(p => p.Id == projectId);
+            }
+
+            return _context.RetropsectiveProjects.FirstOrDefault(p => p.Id == projectId);
+        }
+
+        public IEnumerable<RetropsectiveProject> GetProjectByDate(DateTime date)
+        {
+            return _context.RetropsectiveProjects
+                .Include( f=>f.Feedbacks)
+                .Where(p => p.Date.Date == date.Date)
+                .ToList();
+        }
+
+        public IEnumerable<RetropsectiveProject> GetProjects()
+        {
+            return _context.RetropsectiveProjects.Include(f => f.Feedbacks)
+                .OrderBy(p => p.Id).ToList();
+        }
+
+        public bool ProjectExist(int id)
+        {
+            return _context.RetropsectiveProjects.Any(p => p.Id == id);
+        }
+
+        public bool ProjectNameExist(string name)
+        {
+            return _context.RetropsectiveProjects.Any(p => p.Name == name);
+        }
+
+        public bool Save()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
