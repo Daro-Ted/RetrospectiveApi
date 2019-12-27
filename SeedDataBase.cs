2c2d65dd4e8365b590223a4d449cd17f4632ed41
HO-Ted <da_rossy@hotmail.com>
2019-12-27 09:24:31 +0000

    Add files via upload

---
+++ SeedDataBase.cs
@@ -0,0 +1,120 @@
+﻿using RetrospectiveApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrospectiveApi
+{
+    public static class SeedDataBase
+    {
+
+        public static void EnsureDatabaseSeed(this ProjectDBContext context)
+        {
+            if (context.RetropsectiveProjects.Any())
+            {
+                return;
+            }
+
+            var retrospectiveProject = new List<RetropsectiveProject>()
+            {
+                 new RetropsectiveProject()
+                 {
+                     Name="First Project",
+                     Summary="Create an application that tracks all project commemnts and outcomes",
+                     Date=DateTime.Now,
+                     Participants="Tim Test",
+                     Feedbacks = new List<Feedback>()
+                     {
+                         new Feedback
+                         {
+                            Name="Bright Tan",
+                            Body="Impressive project and outcome was brilliant.",
+                            Types="Postive."
+                         },
+                         new Feedback
+                         {
+                            Name="Jame Can",
+                            Body="I was impressed by project and outcome it was brilliant.",
+                            Types="Postive."
+                         },
+                         new Feedback
+                         {
+                            Name="Friday Ram",
+                            Body="I was impressed by project and outcome it was brilliant, we can improve more to take it to the next level.",
+                            Types="Postive."
+                         },
+                     }
+                 },
+                 new RetropsectiveProject()
+                 {
+                     Name="Second Project",
+                     Summary="Create an application Produce daily report",
+                     Date=DateTime.Now.AddDays(2),
+                     Participants="Tim Test, Harry Test",
+                     Feedbacks = new List<Feedback>()
+                     {
+                         new Feedback
+                         {
+                            Name="Bright Tan",
+                            Body="Impressive project and outcome was brilliant.",
+                            Types="Postive."
+                         },
+                         new Feedback
+                         {
+                            Name="Jame Can",
+                            Body="I was impressed by project and outcome it was brilliant.",
+                            Types="Postive."
+                         },
+                         new Feedback
+                         {
+                            Name="Friday Ram",
+                            Body="I was impressed by project and outcome it was brilliant, we can improve more to take it to the next level.",
+                            Types="Postive."
+                         },
+                     }
+                 },
+                 new RetropsectiveProject()
+                 {
+                     Name="Third Project",
+                     Summary="Create an application that tracks all Parts and Inventory",
+                     Date=DateTime.Now.AddDays(4),
+                     Participants="Tim Ogbe, Test Obi",
+                     Feedbacks = new List<Feedback>()
+                     {
+                         new Feedback
+                         {
+                            Name="Bright Tan",
+                            Body="Impressive project and outcome was brilliant.",
+                            Types="Postive."
+                         },
+                         new Feedback
+                         {
+                            Name="Jame Can",
+                            Body="I was impressed by project and outcome it was brilliant.",
+                            Types="Postive."
+                         },
+                         new Feedback
+                         {
+                            Name="Friday Ram",
+                            Body="I was impressed by project and outcome it was brilliant, we can improve more to take it to the next level.",
+                            Types="Postive."
+                         },
+                     }
+                 },
+                  new RetropsectiveProject()
+                 {
+                     Name="Fourth Project",
+                     Summary="Create an application that tracks all Parts and Inventory",
+                     Date=DateTime.Now.AddDays(4),
+                     Participants="Tim Test, Harry Chan",
+                     Feedbacks = new List<Feedback>()
+                 }
+            };
+            context.RetropsectiveProjects.AddRange(retrospectiveProject);
+            context.SaveChangesAsync();
+        }
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,1 +1,94 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Logging;
+using RetrospectiveApi.MappingProfile;
+using RetrospectiveApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using RetrospectiveApi.Repositories;
+using RetrospectiveApi.Services;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using NLog.Extensions.Logging;
 
+namespace RetrospectiveApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Add cros to enable angular to communicate with the Endpoints
+            services.AddCors(option => option.AddPolicy("ApiCorsPolicy", builder => {
+                builder.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+            }));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //Configure Xml return result and return object with thesame case as models
+            services.AddControllers().AddNewtonsoftJson(o =>
+            {
+                if (o.SerializerSettings.ContractResolver == null) return;
+                var castedReslover = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                castedReslover.NamingStrategy = null;
+            }).AddMvcOptions(x => x.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
+
+            //Connection string
+            var connectionStrings = Configuration.GetConnectionString("RetrospectiveConnectionString");
+            services.AddDbContext<ProjectDBContext>(o => o.UseSqlServer(connectionStrings));
+            //register Repository and services
+            services.AddScoped<IRetrospectiveRepository, RetrospectiveRepository>(); 
+            services.AddScoped<IRetrospectiveService, RetrospectiveService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+     }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ProjectDBContext dbcontext,ILoggerFactory loggerFactory)
+        {
+            //Create Logger to log errors
+            loggerFactory.AddNLog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("ApiCorsPolicy");
+
+            dbcontext.EnsureDatabaseSeed();
+
+            AutoMapper.Mapper.Initialize(cfg => cfg.AddProfile<MapProfile>());
+            
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
